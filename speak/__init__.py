c39f3c82c31865edb92648e1d40aafd3690f0d38
Brains <100498839+King0fBrains@users.noreply.github.com>
2024-03-07 17:14:13 -0500

    Reformat File.

--- speak/__init__.py
+++ speak/__init__.py
@@ -17,9 +17,9 @@ if not importlib.util.find_spec("laggron_utils"):
 
 log = logging.getLogger("red.laggron.say")
 
-
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+
 
 async def setup(bot):
     init_logger(log, Speak.__class__.__name__)
