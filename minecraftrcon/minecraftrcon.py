2b04021f54b3af02e7efaacb9aaa8fec4fb77ca1
Brains <100498839+King0fBrains@users.noreply.github.com>
2024-04-15 21:30:13 -0400

    Add channel messages and whitelist, will need fixes once able to be tested.

--- minecraftrcon/minecraftrcon.py
+++ minecraftrcon/minecraftrcon.py
@@ -2,7 +2,7 @@ import discord
 
 from redbot.core import commands, Config
 from redbot.core.utils.predicates import MessagePredicate
-from mctools import RCONClient
+from mctools import RCONClient, PINGClient, QUERYClient
 
 
 class MinecraftRCON(commands.Cog):
@@ -14,7 +14,9 @@ class MinecraftRCON(commands.Cog):
             "host": "",
             "port": 0,
             "password": "",
-            "admins": []
+            "admins": [],
+            "rcon_channel": [],
+            "whitelist": {}
         }
         self.config.register_guild(**default_guild)
 
@@ -98,6 +100,39 @@ class MinecraftRCON(commands.Cog):
             await ctx.send("Password set successfully. Please delete your message manually.")
 
     @setup.command()
+    async def channel(self, ctx, channel: discord.TextChannel):
+        """Set the channel for RCON commands."""
+        admins = await self.config.guild(ctx.guild).admins()
+        if ctx.author.id not in admins:
+            return await ctx.send("You do not have permission to run this command.")
+        message = MessagePredicate.lower_contained_in(["add", "a", "remove", "r", "cancel", "c"])
+        embed = discord.Embed(title="Add/Remove Admin",
+                              description=f"Would you like to add or remove {channel.mention} as a RCON message channel?",
+                              color=await ctx.embed_color())
+        embed.set_footer(
+            text="Type 'add' to add the channel as an admin, 'remove' to remove it, or 'cancel' to cancel.")
+        await ctx.send(embed=embed)
+        try:
+            await self.bot.wait_for("message", check=message, timeout=10)
+        except TimeoutError:
+            await ctx.send("You took too long to respond.")
+            return
+        if message.result in [0, 1]:
+            async with self.config.guild(ctx.guild).rcon_channel() as channels:
+                if channel.id in channels:
+                    return await ctx.send("This channel is already included")
+                channels.append(channel.id)
+            return await ctx.send(f"{channel.mention} is now a RCON message channel.")
+        elif message.result in [2, 3]:
+            async with self.config.guild(ctx.guild).rcon_channel() as channels:
+                if channel.id not in channels:
+                    return await ctx.send("This channel is not a RCON message channel.")
+                channels.remove(channel.id)
+            return await ctx.send("This channel is no longer a RCON message channel.")
+        else:
+            return await ctx.send("Cancelled.")
+
+    @setup.command(aliases=["showsettings"])
     async def settings(self, ctx):
         """Show the current RCON settings."""
         admins = await self.config.guild(ctx.guild).admins()
@@ -117,7 +152,7 @@ class MinecraftRCON(commands.Cog):
 
     @setup.command()
     async def admin(self, ctx, user: discord.Member):
-        """Add an admin to the RCON connection."""
+        """RCON Admin management."""
         admins = await self.config.guild(ctx.guild).admins()
         if ctx.author.id not in admins and not await self.bot.is_admin(ctx.author) and not await self.bot.is_owner(
                 ctx.author):
@@ -149,10 +184,6 @@ class MinecraftRCON(commands.Cog):
         else:
             return await ctx.send("Cancelled.")
 
-        # admins.append(user.id)
-        # await self.config.guild(ctx.guild).admins.set(admins)
-        # await ctx.send("Admin added successfully.")
-
     @setup.command()
     async def rcon_clear_config(self, ctx):
         """Clear the RCON config."""
@@ -163,7 +194,138 @@ class MinecraftRCON(commands.Cog):
         await self.config.guild(ctx.guild).clear()
         await ctx.send("Config cleared successfully.")
 
+    @rcon.command()
+    async def ping(self, ctx):
+        """Ping the server to check if it's online."""
+        await ctx.typing()
+        host = await self.config.guild(ctx.guild).host()
+        client = PINGClient(host)
+        try:
+            await ctx.send(f"Server is online. Response time: {client.ping()}ms")
+        except ConnectionRefusedError:
+            await ctx.send("Server is offline.")
 
+    @rcon.command()
+    async def players(self, ctx):
+        """Get the players on the server."""
+        host = await self.config.guild(ctx.guild).host()
+        query = QUERYClient(host)
+
+        try:
+            stats = query.get_full_stats()
+            embed = discord.Embed(title=f"Players on {host}", color=await ctx.embed_color())
+            embed.add_field(name="Players", value=", ".join(stats['players']).strip('\x1b[0m'))
+            embed.add_field(name="Player Count", value=stats['numplayers'] + "/" + stats['maxplayers'])
+            await ctx.send(embed=embed)
+        except ConnectionRefusedError:
+            await ctx.send("Server is offline.")
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.channel.id not in await self.config.guild(message.guild).rcon_channel():
+            return
+        if message.author.bot:
+            return
+        if message.channel.id != await self.config.guild(message.guild).rcon_channel():
+            return
+        if message.content.startswith("!rcon"):
+            return await message.channel.send("Please don't use the RCON commands in this channel.")
+        if message.content.startswith("!"):
+            return
+        msg = message.content
+        if "{password}" in msg:
+            async with self.config.guild(message.guild).admin() as admins:
+                x = [message.guild.get_member(a) for a in admins]
+            return await message.channel.send("Please don't include the password in your message. " +
+                                              ", ".join([i.mention for i in x]))
+
+        host = await self.config.guild(message.guild).host()
+        port = await self.config.guild(message.guild).port()
+        password = await self.config.guild(message.guild).password()
+        rcon = RCONClient(host, port=port)
+        try:
+            if rcon.login(password):
+                password = "**********"
+                msg = "say " + msg + "Sent by" + message.author.name
+                resp = rcon.command(msg).strip("[0m")
+                if resp in self.valid_responses:
+                    await message.add_reaction("✅")
+                else:
+                    await message.channel.send(f"Response: {resp}")
+            else:
+                await message.channel.send("Failed to login to RCON.")
+        except ConnectionRefusedError:
+            await message.channel.send(
+                "Failed to connect to RCON. Check that the server is online and the RCON port is "
+                "correct.")
+        except Exception as e:
+            await message.channel.send(f"An error occurred. Please try again later.")
+            raise e
+
+    @commands.group()
+    async def whitelist(self, ctx):
+        """Whitelist management commands."""
+        pass
+
+    @whitelist.command()
+    async def add(self, ctx, user: str):
+        """Add a user to the whitelist."""
+        host = await self.config.guild(ctx.guild).host()
+        port = await self.config.guild(ctx.guild).port()
+        password = await self.config.guild(ctx.guild).password()
+        rcon = RCONClient(host, port=port)
+        async with self.config.guild(ctx.guild).whitelist() as whitelist:
+            if ctx.author.id in whitelist:
+                return await ctx.send("You are already on the whitelist.")
+            try:
+                if rcon.login(password):
+                    resp = rcon.command(f"whitelist add {user}").strip("[0m")
+                    if resp in self.valid_responses:
+                        await ctx.send(f"{user} has been added to the whitelist.")
+                        whitelist[str(ctx.author.id)] = user
+                    else:
+                        await ctx.send(f"Response: {resp}")
+                else:
+                    await ctx.send("Failed to login to RCON.")
+            except ConnectionRefusedError:
+                await ctx.send("Failed to connect to RCON. Check that the server is online and the RCON port is "
+                               "correct.")
+
+    @whitelist.command()
+    async def remove(self, ctx, user: str):
+        """Remove a user from the whitelist."""
+        host = await self.config.guild(ctx.guild).host()
+        port = await self.config.guild(ctx.guild).port()
+        password = await self.config.guild(ctx.guild).password()
+        rcon = RCONClient(host, port=port)
+        async with self.config.guild(ctx.guild).admins() as admins:
+            if ctx.author.id not in admins:
+                return await ctx.send("You do not have permission to run this command.")
+        async with self.config.guild(ctx.guild).whitelist() as whitelist:
+            try:
+                if rcon.login(password):
+                    resp = rcon.command(f"whitelist remove {user}").strip("[0m")
+                    if resp in self.valid_responses:
+                        await ctx.send(f"{user} has been removed from the whitelist.")
+                        whitelist.pop(str(ctx.author.id))
+                    else:
+                        await ctx.send(f"Response: {resp}")
+                else:
+                    await ctx.send("Failed to login to RCON.")
+            except ConnectionRefusedError:
+                await ctx.send("Failed to connect to RCON. Check that the server is online and the RCON port is "
+                               "correct.")
+
+    @whitelist.command()
+    async def show(self, ctx):
+        """Display the whitelist."""
+        async with self.config.guild(ctx.guild).whitelist() as whitelist:
+            if not whitelist:
+                return await ctx.send("The whitelist is empty.")
+            embed = discord.Embed(title="Whitelist", color=await ctx.embed_color())
+            for k, v in whitelist.items():
+                embed.add_field(name=k, value=v)
+            await ctx.send(embed=embed)
 
 
 async def setup(bot):
