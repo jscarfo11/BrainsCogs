dbc5fa9270369e9b89e7cee75f3d446205f72b20
Brains <100498839+King0fBrains@users.noreply.github.com>
2024-04-18 10:08:04 -0400

    Add moveset command.

---
+++ pokedex/helpers.py
@@ -0,0 +1,15 @@
+import discord
+import textwrap
+
+from discord.ext import commands
+
+
+async def construct_embed(ctx: commands.Context, index: dict, embed: discord.Embed):
+    for element, value in index.items():
+        if value and len(str(value)) <= 1024:
+            embed.add_field(name=element, value="\n".join(textwrap.wrap(str(value), 25)))
+        elif value and len(str(value)) > 1024:
+            embed.add_field(name=element, value="Value too long to display.")
+        else:
+            embed.add_field(name=element, value="N/A")
+
--- pokedex/pokedex.py
+++ pokedex/pokedex.py
@@ -4,6 +4,7 @@ import pokebase as pb
 import textwrap
 
 from redbot.core import commands
+from .helpers import construct_embed
 
 
 class Pokedex(commands.Cog):
@@ -14,7 +15,6 @@ class Pokedex(commands.Cog):
     async def pokedex(self, ctx):
         """Get information about Pokemon."""
         pass
-
 
     @pokedex.command()
     async def pokemon(self, ctx, pokemon):
@@ -92,14 +92,7 @@ class Pokedex(commands.Cog):
             "Flavor Text": ability.flavor_text_entries[1].flavor_text,
             "Learned By": ", \n".join(learns)
         }
-        for element, value in index.items():
-            if value and len(str(value)) <= 1024:
-                embed.add_field(name=element, value="\n".join(textwrap.wrap(str(value), 25)))
-            elif value and len(str(value)) > 1024:
-                embed.add_field(name=element, value="Value too long to display.")
-            else:
-                embed.add_field(name=element, value="N/A")
-
+        await construct_embed(ctx, index, embed)
         await ctx.send(embed=embed)
 
     @pokedex.command()
@@ -126,20 +119,43 @@ class Pokedex(commands.Cog):
             'Learned By': ", \n".join(learn)
 
         }
-        for element, value in index.items():
-            if value and len(str(value)) <= 1024:
-                embed.add_field(name=element, value="\n".join(textwrap.wrap(str(value), 25)))
-            elif value and len(str(value)) > 1024:
-                embed.add_field(name=element, value="Value too long to display.")
-            else:
-                embed.add_field(name=element, value="N/A")
+        await construct_embed(ctx, index, embed)
 
         await ctx.send(embed=embed)
 
+    @pokedex.command(aliases=["moves", "movepool", "move_set"])
+    async def moveset(self, ctx, pokemon):
+        """Get the moveset of a Pokemon by name or ID."""
+        try:
+            pokemon = pb.pokemon(pokemon)
+            assert pokemon.id
+        except requests.exceptions.HTTPError:
+            return await ctx.send("Pokemon not found. Please check your spelling and try again.")
+        except AttributeError:
+            return await ctx.send("Pokemon not found. Please check your spelling and try again.")
+        moves = []
+        for i in pokemon.moves:
+            moves.append([i.move.name.capitalize(), i.version_group_details[0].level_learned_at, i])
+        levelup = []
+        tutor = []
+        machine = []
+        for move in moves:
+            if move[2].version_group_details[0].move_learn_method.name == "level-up":
+                levelup.append([move[0], move[1]])
+            elif move[2].version_group_details[0].move_learn_method.name == "machine":
+                machine.append(f"{move[0]}")
+            elif move[2].version_group_details[0].move_learn_method.name == "tutor":
+                tutor.append(f"{move[0]}")
 
+        levelup = sorted(levelup, key=lambda x: x[1])
+        levelup = [f"{i[0]} at level {i[1]}" for i in levelup]
 
+        embed = discord.Embed(title=f"{pokemon.name.capitalize()}", color=await ctx.embed_color())
+        embed.add_field(name="Level-up", value="\n".join(levelup))
+        embed.add_field(name="Machine", value="\n".join(machine))
+        embed.add_field(name="Tutor", value="\n".join(tutor))
 
-
+        await ctx.send(embed=embed)
 
 
 async def setup(bot):
