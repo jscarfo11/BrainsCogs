3a3f36798627cb4e6ec76c6974a4dd588a8b586a
King0fBrains <100498839+King0fBrains@users.noreply.github.com>
2024-06-19 21:10:08 -0400

    Try this?

--- legacy/legacy.py
+++ legacy/legacy.py
@@ -1,4 +1,5 @@
 import discord
+from redbot.cogs.mod.utils import is_allowed_by_hierarchy
 from redbot.core import commands
 
 
@@ -11,11 +12,11 @@ class TouchableMember(commands.MemberConverter):
 class Legacy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.role = ""
+        self.role = 1169638210205388920
 
-    @commands.has_any_role()
+    @commands.has_any_role(1188264672370098207, 878727109143580683, 810013892670521364)
     @commands.command()
-    async def legacy(self, ctx, user: TouchableMember):
+    async def legacy(self, ctx, user: discord.Member)
         """This is a legacy command."""
-        await user.add_roles(self.role)
-        await ctx.send(f"Added role to {user.display_name}.")
+        await self.bot.add_roles(user, self.role)
+        await ctx.send(f"Added the role to {user.mention}.")
