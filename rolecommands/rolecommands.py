84d67b57575f6f70057cd8450f6d86c6f1a6f50c
Jack Scarfo <jackdscarfo@gmail.com>
2025-01-02 19:50:07 -0500

    Add nerd role

--- rolecommands/rolecommands.py
+++ rolecommands/rolecommands.py
@@ -6,7 +6,7 @@ class RoleCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.role_legacy = 1169638210205388920
-        self.role_christmas = 1312839539647189263
+        self.role_nerd = 1324475936451727453
 
     @commands.has_any_role(1188264672370098207, 878727109143580683, 810013892670521364)
     @commands.command()
@@ -21,15 +21,18 @@ class RoleCommands(commands.Cog):
 
     @commands.has_any_role(1188264672370098207, 878727109143580683, 810013892670521364)
     @commands.command(aliases=["xmas", "christmasrole", "christmas"])
-    async def merrychristmas(self, ctx, user: discord.Member):
-        """This adds the christmas role."""
-        try:
-            role = discord.utils.get(ctx.guild.roles, id=self.role_christmas)
-            role_name = role.name
-        except AttributeError:
-            return await ctx.send("Role not found.")
-        if role in user.roles:
-            return await ctx.send(f"{user.name} already has the role.")
+    async def nerd(self, ctx, users: commands.Greedy[discord.Member]):
+        """This adds the nerd role."""
+        for user in users:
+            try:
+                role = discord.utils.get(ctx.guild.roles, id=self.role_nerd)
+                role_name = role.name
+            except AttributeError:
+                await ctx.send("Role not found.")
+                continue
+            if role in user.roles:
+                await ctx.send(f"{user.name} already has the role.")
+                continue
 
-        await user.add_roles(role)
-        await ctx.send(f"Added **{role_name}** to **{user.name}**.")
+            await user.add_roles(role)
+            await ctx.send(f"Added **{role_name}** to **{user.name}**.")
